c67b4ecc474f2ab60e31e0b480b15b83b1c13188
dvankevich <dvankevich@gmail.com>
2025-12-06 13:57:12 +0200

    remove destructurization in Task

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,19 +1,19 @@
 import css from './Task.module.css';
 
-export default function Task({ data: { id, text }, onDelete, onEdit }) {
-  console.log(id, text);
+export default function Task({ data, onDelete, onEdit }) {
+  //console.log(data);
 
   const handleEdit = () => {
-    const newText = prompt('Edit task text:', text);
+    const newText = prompt('Edit task text:', data.text);
     if (newText) {
-      onEdit({ id: id, text: newText });
+      onEdit({ ...data, text: newText });
     }
   };
 
   return (
     <div className={css.container}>
-      <p className={css.text}>{text}</p>
-      <button className={css.btn} onClick={() => onDelete(id)}>
+      <p className={css.text}>{data.text}</p>
+      <button className={css.btn} onClick={() => onDelete(data.id)}>
         Delete
       </button>
       <button className={css.btn} onClick={handleEdit}>
