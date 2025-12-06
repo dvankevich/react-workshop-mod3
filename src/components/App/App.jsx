d82ed7b04b4cdd3b737668906b75a2b79c88ca77
dvankevich <dvankevich@gmail.com>
2025-12-06 11:38:02 +0200

    save task to local storage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,25 +3,33 @@ import initialTasks from '../../tasks.json';
 import Form from '../Form/Form';
 import Filter from '../Filter/Filter';
 import css from './App.module.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function App() {
-  const [tasks, setTasks] = useState(initialTasks);
+  const [tasks, setTasks] = useState(() => {
+    const savedTasks = localStorage.getItem('tasks');
+    return savedTasks ? JSON.parse(savedTasks) : initialTasks;
+  });
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
   const [filter, setFilter] = useState('');
 
-  const addTask = (newTask) => {
-    setTasks((prevTasks) => {
+  const addTask = newTask => {
+    setTasks(prevTasks => {
       return [...prevTasks, newTask];
     });
   };
 
-  const deleteTask = (taskId) => {
-    setTasks((prevTasks) => {
-      return prevTasks.filter((task) => task.id !== taskId);
+  const deleteTask = taskId => {
+    setTasks(prevTasks => {
+      return prevTasks.filter(task => task.id !== taskId);
     });
   };
 
-  const visibleTasks = tasks.filter((task) =>
+  const visibleTasks = tasks.filter(task =>
     task.text.toLowerCase().includes(filter.toLowerCase())
   );
 
